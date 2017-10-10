fabc877b5c5a3e0036bf2d767266924163482ef9
Gulshan Raj <gulshanraj116@gmail.com>
2017-10-10 22:04:36 +0530

    "calculator input string formatted"

---
+++ Numbers/calculator/script.js
@@ -0,0 +1,39 @@
+var calculator = {
+  operateOn: function(input) {
+    var inputAr = this.format(input);
+    var postfixStr = this.infixToPostfix(inputAr);
+    return this.evaluatePostfix(postfixStr);
+  },
+  format: function(input) {
+    //convert given string into infix array
+    input = input.replace(/[\+\-\*\/\(\)]/g, ' $& ').replace(/  +/g, ' '); //place single spaces around operators
+    inputAr = input.split(' ').filter( (w) => w != '');
+    return inputAr;
+  },
+  infixToPostfix: function() {
+    //TODO convert infix to postfix
+  },
+  evaluatePostfix: function() {
+    //TODO solve the expression
+  }
+};
+
+var handler = {
+  clear: function() {
+    //clear everything
+    view.displayResult('');
+  },
+  getAns: function() {
+    //handle given expression
+    var calculatorDisplay = document.getElementById('calculatorDisplay');
+    var calculatorDisplayOutput = calculator.operateOn(calculatorDisplay.value);
+    displayResult(calculatorDisplayOutput);
+  }
+};
+
+var view = {
+  displayResult: function(displayString) {
+    var calculatorDisplay = document.getElementById('calculatorDisplay');
+    calculatorDisplay.value = displayString;
+  }
+};
